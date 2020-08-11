9cf0b6b6f7e2c9ac09c1c2f58bcbf46b1f2ea049
yangyang <1296572091@qq.com>
2020-08-11 15:37:26 +0800

    update ConditionalRandomField

--- transformers/ConditionalRandomField.py
+++ transformers/ConditionalRandomField.py
@@ -1,5 +1,54 @@
 import torch
 from torch import nn
+import torch.nn.init as init
+
+def initial_parameter(net, initial_method=None):
+    r"""A method used to initialize the weights of PyTorch models.
+
+    :param net: a PyTorch model
+    :param str initial_method: one of the following initializations.
+
+            - xavier_uniform
+            - xavier_normal (default)
+            - kaiming_normal, or msra
+            - kaiming_uniform
+            - orthogonal
+            - sparse
+            - normal
+            - uniform
+
+    """
+    init_method = init.xavier_normal_
+    def weights_init(m):
+        # classname = m.__class__.__name__
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv1d) or isinstance(m, nn.Conv3d):  # for all the cnn
+            if initial_method is not None:
+                init_method(m.weight.data)
+            else:
+                init.xavier_normal_(m.weight.data)
+            init.normal_(m.bias.data)
+        elif isinstance(m, nn.LSTM):
+            for w in m.parameters():
+                if len(w.data.size()) > 1:
+                    init_method(w.data)  # weight
+                else:
+                    init.normal_(w.data)  # bias
+        elif m is not None and hasattr(m, 'weight') and \
+                hasattr(m.weight, "requires_grad"):
+                if len(m.weight.size()) > 1:
+                    init_method(m.weight.data)
+                else:
+                    init.normal_(m.weight.data)  # batchnorm or layernorm
+        else:
+            for w in m.parameters():
+                if w.requires_grad:
+                    if len(w.data.size()) > 1:
+                        init_method(w.data)  # weight
+                    else:
+                        init.normal_(w.data)  # bias
+                # print("init else")
+    
+    net.apply(weights_init)
 
 class ConditionalRandomField(nn.Module):
     r"""
